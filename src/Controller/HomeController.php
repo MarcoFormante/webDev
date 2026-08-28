1a554390748deccfd1450f1618d8b265d7b27b41
Marco <formante.marco@gmail.com>
2026-08-28 11:42:47 +0200

    get projects from DB

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,26 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods:["GET"])]
-    public function index(): Response
+    public function index(ProjectRepository $pr): Response
     {
+        $projects = [];
+        $data = $pr->findBy(["isActive" => 1],["position" => "ASC"]);
+        if ($data) {
+            foreach ($data as $project) {
+                $projects[] = [
+                    "bgColor" => $project->getBgColor(),
+                    "textColor" => $project->getTextColor(),
+                    "slug" => $project->getSlug(),
+                    "description" => $project->getDescription(),
+                    "title" => $project->getTitle(),
+                    "stack" => $project->getStack(),
+                    "position" => $project->getPosition(),
+                ];
+            }
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'projects' => $projects,
         ]);
     }
 }
