88b3cfe491c166fee151237ca42a6b948aeb29e2
Marco <formante.marco@gmail.com>
2026-09-03 16:55:56 +0200

    edit DB Columns, add color and description it|fr|en

---
+++ migrations/Version20260903134750.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260903134750 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project ADD description_fr VARCHAR(255) NOT NULL, ADD description_en VARCHAR(255) NOT NULL, CHANGE description description_it VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE project ADD description VARCHAR(255) NOT NULL, DROP description_it, DROP description_fr, DROP description_en');
+    }
+}
